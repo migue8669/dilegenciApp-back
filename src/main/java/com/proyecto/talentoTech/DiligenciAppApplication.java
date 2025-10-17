645685fcb328a4d7b4d467e1e2ace5918b8a3598
migue8669 <miguelramirez8669@gmail.com>
2025-10-16 20:53:15 -0500

    creación de proyecto back

---
+++ src/main/java/com/proyecto/talentoTech/DiligenciAppApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.talentoTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiligenciAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiligenciAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/talentoTech/DiligenciAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.talentoTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiligenciAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
